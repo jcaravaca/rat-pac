d0a06488089e014e825776e1dcc274047ec07ad7
jcaravaca <jcaravaca@berkeley.edu>
2015-02-12 11:07:27 -0800

    Add DAQ processor. It doesn't do the proper thing yet, this is just here for backup

---
+++ src/daq/DAQProc.cc
@@ -0,0 +1,94 @@
+#include <vector>
+#include <RAT/DAQProc.hh>
+#include <RAT/DB.hh>
+#include <G4ThreeVector.hh>
+#include <RAT/DetectorConstruction.hh>
+
+using namespace std;
+
+namespace RAT {
+
+DAQProc::DAQProc() : Processor("daq") {
+  //DBLinkPtr ldaq = DB::Get()->GetLink("DAQ");
+  //fSPECharge = ldaq->GetDArray("SPE_charge"); // convert pC to gain-normalized units
+  fEventCounter = 0;
+}
+
+Processor::Result DAQProc::DSEvent(DS::Root *ds) {
+  // This simple simulation assumes only tubes hit by a photon register
+  // a hit, and that every MC event corresponds to one triggered event
+  // The time of the PMT hit is that of the first photon.
+
+  DS::MC *mc = ds->GetMC();
+  if(ds->ExistEV()) {  // there is already a EV branch present 
+    ds->PruneEV();     // remove it, otherwise we'll have multiple detector events
+                       // in this physics event
+                       // we really should warn the user what is taking place		    
+  }
+  DS::EV *ev = ds->AddNewEV();
+  
+  ev->SetID(fEventCounter);
+  fEventCounter++;
+
+  double totalQ = 0.0;
+  double calibQ = 0.0;
+
+
+  //Check if trigger PMT was hit
+  bool triggerON = false;
+  DS::MCPMT *mcpmt; //HQE PMT
+  for (int imcpmt=0; imcpmt < mc->GetMCPMTCount(); imcpmt++) {
+
+    if(mc->GetMCPMT(imcpmt)->GetID()==1){ //Is the trigger PMT
+      if (mcpmt->GetMCPhotonCount() > 0) triggerON = true;
+    } else{
+
+      mcpmt = mc->GetMCPMT(imcpmt);
+
+    }
+    
+    // DS::MCPMT *mcpmt = mc->GetMCPMT(imcpmt);
+    // int pmtID = mcpmt->GetID();
+    
+  }
+  
+  if(triggerON){
+    
+    if (mcpmt->GetMCPhotonCount() > 0) {
+      // Need at least one photon to trigger
+      DS::PMT* pmt = ev->AddNewPMT();
+      pmt->SetID(0);
+      
+      // Create one sample, hit time is determined by first hit,
+      // "infinite" charge integration time
+      // WARNING: gets multiphoton effect right, but not walk correction
+      // Write directly to calibrated waveform branch
+      
+      double time = mcpmt->GetMCPhoton(0)->GetHitTime();
+      double charge = 0;
+      
+      for (int i=0; i < mcpmt->GetMCPhotonCount(); i++)  {
+	if (time > mcpmt->GetMCPhoton(i)->GetHitTime())
+	  time = mcpmt->GetMCPhoton(i)->GetHitTime();
+	charge += mcpmt->GetMCPhoton(i)->GetCharge();
+      }
+      
+      //pmt->SetCalibratedCharge(charge);
+      totalQ += charge;
+      
+      //charge *= fSPECharge[pmtID] * 1e12; /* convert to pC */
+      pmt->SetTime(time);
+      pmt->SetCharge(charge);
+      calibQ += charge;
+      
+    }
+  }
+  
+  ev->SetTotalCharge(totalQ);
+  //ev->SetCalibQ(calibQ);
+  
+  return Processor::OK;
+}
+
+} // namespace RAT
+
